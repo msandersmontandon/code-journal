02657dccd8e5812e4af0e7d8eff2e0b2920fcc2b
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-04-05 22:44:36 +0000

    Test all functionality again in a private brownsing session.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+if (localStorage.getItem('data')) {
+  for (var key in data) {
+    var previousDataJSON = localStorage.getItem('data');
+    var previousData = JSON.parse(previousDataJSON);
+    data[key] = previousData[key];
+  }
+}
+
+window.addEventListener('unload', function (event) {
+  localStorage.setItem('data', JSON.stringify(data));
+});
--- js/main.js
+++ js/main.js
@@ -6,49 +6,25 @@ var $entryUrl = document.querySelector('#entry-url');
 var $entryTitle = document.querySelector('#entry-title');
 var $entryNotes = document.querySelector('#entry-notes');
 var $entryImage = document.querySelector('.entry-image > img');
-var photoUrl = '';
-var photoTitle = '';
-var notes = '';
-
-if (localStorage.getItem('data')) {
-  for (var key in data) {
-    var previousDataJSON = localStorage.getItem('data');
-    var previousData = JSON.parse(previousDataJSON);
-    data[key] = previousData[key];
-  }
-}
-
-$entryForm.addEventListener('input', function (event) {
-  if (event.target === $entryUrl) {
-    photoUrl = event.target.value;
-  }
-  if (event.target === $entryTitle) {
-    photoTitle = event.target.value;
-  }
-  if (event.target === $entryNotes) {
-    notes = event.target.value;
-  }
-});
-
 $entryUrl.addEventListener('blur', function (event) {
-  if (photoUrl) {
-    $entryImage.setAttribute('src', photoUrl);
+  if (event.target.value) {
+    $entryImage.setAttribute('src', event.target.value);
   }
 });
 
 $entryTitle.addEventListener('blur', function (event) {
-  if (photoTitle) {
-    $entryImage.setAttribute('alt', photoTitle);
-    $entryImage.setAttribute('title', photoTitle);
+  if (event.target.value) {
+    $entryImage.setAttribute('alt', event.target.value);
+    $entryImage.setAttribute('title', event.target.value);
   }
 });
 
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
   var entryFormObject = {
-    entryTitle: photoTitle,
-    entryUrl: photoUrl,
-    entryNotes: notes,
+    entryTitle: $entryTitle.value,
+    entryUrl: $entryUrl.value,
+    entryNotes: $entryNotes.value,
     nextEntryId: data.nextEntryId,
     entryId: data.nextEntryId - 1
   };
@@ -57,14 +33,5 @@ $entryForm.addEventListener('submit', function (event) {
   $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryImage.setAttribute('alt', 'Placeholder Image');
   $entryImage.setAttribute('title', 'Placeholder Image');
-  $entryUrl.value = '';
-  $entryTitle.value = '';
-  $entryNotes.value = '';
-  photoUrl = '';
-  photoTitle = '';
-  notes = '';
-  var entryFormJSON = JSON.stringify(entryFormObject);
-  var key = 'entry-form-' + entryFormObject.entryId;
-  localStorage.setItem(key, entryFormJSON);
-  localStorage.setItem('data', JSON.stringify(data));
+  document.querySelector('form').reset();
 });
